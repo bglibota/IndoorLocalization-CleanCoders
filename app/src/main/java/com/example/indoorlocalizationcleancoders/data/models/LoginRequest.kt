f51fdaaf115059948e05aade2e12ae8b0c466036
drusak21 <drusak21@student.foi.hr>
2024-12-09 16:06:30 +0100

    add data models for LoginRequest and RegisterRequest

---
+++ app/src/main/java/com/example/indoorlocalizationcleancoders/data/models/LoginRequest.kt
@@ -0,0 +1,3 @@
+package com.example.indoorlocalizationcleancoders.data.models
+
+data class LoginRequest(val username: String, val password: String)
---
+++ app/src/main/java/com/example/indoorlocalizationcleancoders/data/models/RegisterRequest.kt
@@ -0,0 +1,7 @@
+package com.example.indoorlocalizationcleancoders.data.models
+
+data class RegisterRequest(
+    val name: String,
+    val username: String,
+    val password: String
+)
