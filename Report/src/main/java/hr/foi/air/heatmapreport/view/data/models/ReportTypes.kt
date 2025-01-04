c249ecc9eeed070c002dc9130a61f59fa7b2a434
Toni Papić <tpapic21@foi.hr>
2025-01-04 15:56:39 +0100

    Pull from develop

--- Report/src/main/java/hr/foi/air/heatmapreport/view/data/models/ReportTypes.kt
+++ Report/src/main/java/hr/foi/air/heatmapreport/view/data/models/ReportTypes.kt
@@ -2,5 +2,5 @@ package hr.foi.air.heatmapreport.view.data.models
 
 enum class ReportTypes(val type: String) {
     HEATMAP_REPORT("Heatmap Report"),
-    ZONE_REPORT("Zone Report"),
+    ZONE_REPORT("Zone Report")
 }
