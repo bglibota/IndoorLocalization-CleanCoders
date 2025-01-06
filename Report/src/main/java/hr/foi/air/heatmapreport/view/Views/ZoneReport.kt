62d44fc5fe6573c6ac60776a598a05b5b8791341
Toni Papić <tpapic21@foi.hr>
2025-01-06 14:54:36 +0100

    Update ZoneReport class, add required constructor parameter

--- Report/src/main/java/hr/foi/air/heatmapreport/view/Views/ZoneReport.kt
+++ Report/src/main/java/hr/foi/air/heatmapreport/view/Views/ZoneReport.kt
@@ -1,10 +1,14 @@
 package hr.foi.air.heatmapreport.view.Views
 
+import android.content.Context
 import androidx.compose.runtime.Composable
 import androidx.navigation.NavController
+import hr.foi.air.heatmapreport.view.ViewModels.ReportGeneratorVM
 import hr.foi.air.heatmapreport.view.interfaces.IReport
 
-class ZoneReport(override var _navController: NavController): IReport {
+class ZoneReport(override var _navController: NavController,
+                 override var sharedReportGeneratorVM: ReportGeneratorVM
+): IReport {
     @Composable
     override fun GetReport() {
         TODO("Not yet implemented")
