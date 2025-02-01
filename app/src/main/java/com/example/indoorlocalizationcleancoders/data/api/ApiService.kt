23d31e8be75bcb55e532636801ccca13d844e51b
drusak21 <drusak21@student.foi.hr>
2025-02-01 17:25:20 +0100

    Update login API response type to Response<LoginResponse>

--- app/src/main/java/com/example/indoorlocalizationcleancoders/data/api/ApiService.kt
+++ app/src/main/java/com/example/indoorlocalizationcleancoders/data/api/ApiService.kt
@@ -1,6 +1,7 @@
 package com.example.indoorlocalizationcleancoders.data.api
 
 import com.example.indoorlocalizationcleancoders.data.models.LoginRequest
+import com.example.indoorlocalizationcleancoders.data.models.LoginResponse
 import com.example.indoorlocalizationcleancoders.data.models.RegisterRequest
 import retrofit2.Response
 import retrofit2.http.Body
@@ -9,7 +10,7 @@ import retrofit2.http.POST
 
 interface ApiService{
     @POST("Login")
-    suspend fun login(@Body request: LoginRequest) : Response<Void>
+    suspend fun login(@Body request: LoginRequest) : Response<LoginResponse>
 
     @POST("Register")
     suspend fun register(@Body request: RegisterRequest) : Response<Void>
