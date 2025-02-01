237fa1de524e4847b6e56ed1a557a01070be9ad9
drusak21 <drusak21@student.foi.hr>
2025-02-01 17:23:13 +0100

    Add data class for LoginResponse

---
+++ app/src/main/java/com/example/indoorlocalizationcleancoders/data/models/LoginResponse.kt
@@ -0,0 +1,8 @@
+package com.example.indoorlocalizationcleancoders.data.models
+
+data class LoginResponse(
+    val id: Int,
+    val name: String?,
+    val username: String?,
+    val roleId: Int
+)
